5e0965ca6659ac2f134ed21dcf595afb5fe35de0
obviouswhy <brew@localhost.localdomain>
2018-10-10 17:29:20 +0900

    Display operation

--- js/main.js
+++ js/main.js
@@ -26,10 +26,18 @@ calcKeys.forEach(key => key.addEventListener('click',(e) => {
     /*operation = operation + key.value;
     console.log(key.value); 
     console.log(operation);*/
-    document.querySelector('.operationDisplay').value = operation;    
+    document.querySelector('.operationDisplay').value = operation;
+    document.querySelector('.operationDisplay').setSelectionRange(0,0);    
 }));
 
 function doCalc(op) {
     console.log(op);
     
+}
+/* Makes the pointer in the display stay in the last digit */
+function rtl(element)
+{   
+    if(element.setSelectionRange){
+        element.setSelectionRange(0,0);
+    }
 }
