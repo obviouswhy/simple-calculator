135546eebe023c46b214f06b2f2b34ac1817597a
obviouswhy <brew@localhost.localdomain>
2018-10-10 17:18:39 +0900

    Display operation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,35 @@
 
 var keyVal;
+var operation = "";
 var calcKeys = document.querySelectorAll('.calc-btn');
 calcKeys.forEach(key => key.addEventListener('click',(e) => {
+    switch (key.value) {
+        case 'AC':
+            operation = "";
+            break;
+        case '=':
+            doCalc(operation);
+            break;
+    /*    case 'AC':
+            operation = "";
+            break;    
+        case 'AC':
+            operation = "";
+            break;
+        case 'AC':
+            operation = "";
+            break;    */
+        default:
+            operation = operation + key.value
+            break;
+    }
+    /*operation = operation + key.value;
     console.log(key.value); 
+    console.log(operation);*/
+    document.querySelector('.operationDisplay').value = operation;    
 }));
+
+function doCalc(op) {
+    console.log(op);
+    
+}
